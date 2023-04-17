d29d4efd89fb04896cbcaab93b2ad3dbd39fde1c
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2023-04-17 21:22:10 +0530

    try2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 import requests
 
 app = Flask(__name__)
+
 @app.route('/currency_converter')
 def currency_converter():
     amount = request.args.get('amount')
@@ -16,21 +17,28 @@ def currency_converter():
         conversion_rate = data['rates'][to_currency]
         converted_amount = float(amount) * conversion_rate
         formatted_amount = '{:.2f}'.format(converted_amount)
-        return f'{amount} {from_currency} is {formatted_amount} {to_currency}'
+        response_data = {
+            "amount": amount,
+            "from_currency": from_currency,
+            "to_currency": to_currency,
+            "converted_amount": converted_amount,
+            "formatted_amount": formatted_amount
+        }
+        return jsonify(response_data)
     else:
-        return f'Unable to convert {from_currency} to {to_currency}'
+        return jsonify({"error": f'Unable to convert {from_currency} to {to_currency}'})
 
 @app.route('/currencies')
 def get_currencies():
     url = 'https://api.exchangerate-api.com/v4/latest/USD'
     response = requests.get(url)
 
-    # if response.status_code == 200:
-    data = response.json()
-    currencies = list(data['rates'].keys())
-    return jsonify(currencies)
-    # else:
-    return 'Unable to retrieve currencies'
+    if response.status_code == 200:
+        data = response.json()
+        currencies = list(data['rates'].keys())
+        return jsonify(currencies)
+    else:
+        return jsonify({"error": "Unable to retrieve currencies"})
 
 if __name__ == '__main__':
     app.run(debug=True)
